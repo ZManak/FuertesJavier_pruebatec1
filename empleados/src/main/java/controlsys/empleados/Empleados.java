d613a07efac00f4318bd88694710c0528b708a29
ZManak <javiefe.dev@gmail.com>
2023-12-27 01:49:21 +0100

    Create Project

---
+++ empleados/src/main/java/controlsys/empleados/Empleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package controlsys.empleados;
+
+/**
+ *
+ * @author Manak
+ */
+public class Empleados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
